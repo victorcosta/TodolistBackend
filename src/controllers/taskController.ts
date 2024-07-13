d749a1cc5b67fd425f8e413b34d4022070d1848f
Joao Victor Dias <victorcostadev@gmail.com.br>
2024-07-13 12:33:28 -0300

    improve AuthRequest interface

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -2,10 +2,17 @@ import { Request, Response } from 'express';
 import Task from '../models/Task';
 
 interface AuthRequest extends Request {
-    user?: any;
+    user: {
+        id: String, 
+    }
+    body: {
+        title: string,
+        completed?: string
+    }
 }
 
 export const createTask = async (req: AuthRequest, res: Response) => {
+    console.log(req);
     const { title } = req.body;
     const userId = req.user.id;
     try {
